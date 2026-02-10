0a3fb309cbb30aa17c19b9fa05edfed2b6651e1f
Flamerider347 <flamerider995@gmail.com>
2026-02-10 13:57:35 +1300

    Added main game scene with character + window resize

---
+++ scripts/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(_delta: float) -> void:
+	var direction_horizontal := Input.get_axis("left", "right")
+	if direction_horizontal:
+		velocity.x = direction_horizontal * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	var direction_vertical := Input.get_axis("up", "down")
+	if direction_vertical:
+		velocity.y = direction_vertical * SPEED
+	else:
+		velocity.y = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
