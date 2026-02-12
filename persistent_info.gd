5761b50ae851265e376c04c9faebeda7d5aff79c
Your Name~ <mthomas4773@gmail.com>
2026-02-12 21:06:38 +1300

    removed old architecture

--- persistent_info.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-var port = -1
-var enet_peer := ENetMultiplayerPeer.new()
--- scripts/client.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-
-func _ready():
-	Globals.enet_peer.create_client("localhost", Globals.port)
-	multiplayer.multiplayer_peer = Globals.enet_peer
