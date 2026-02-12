6d7a042f7ab8bf470257abccb477f79e6d412500
Your Name~ <mthomas4773@gmail.com>
2026-02-13 10:21:41 +1300

    Added server generation code

--- scripts/multiplayer_manager.gd
+++ scripts/multiplayer_manager.gd
@@ -1,11 +1,6 @@
 extends Node
 
-var peer = ENetMultiplayerPeer.new()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+var enet_peer = ENetMultiplayerPeer.new()
 
 
 func create_LAN_server():
@@ -14,17 +9,19 @@ func create_LAN_server():
 	var status = -1
 	while status != 0 and port < 55000:
 		port += 1
-		status = peer.create_server(port, 2)
-	multiplayer.multiplayer_peer = peer
+		status = enet_peer.create_server(port, 2)
+	multiplayer.multiplayer_peer = enet_peer
 	$port.text = "port: " + str(port)
 	multiplayer.peer_connected.connect(spawn_player)
+	$main_menu.queue_free()
+	add_child(preload("res://prefabs/main_level.tscn").instantiate())
 
+func create_client():
+	pass
+	
 func spawn_player(peer_id):
 	var player = preload("res://prefabs/player.tscn").instantiate()
 	player.position = Vector2(350, 200)
 	player.set_multiplayer_authority(peer_id)
 	player.name = str(peer_id)
 	$players.add_child(player)
-	
-	
-	
