10a819c0eaa288a10237b28a13c31e7f483e3570
Your Name~ <mthomas4773@gmail.com>
2026-02-13 13:26:44 +1300

    Made client connectivity - server still controls all players

--- scripts/multiplayer_manager.gd
+++ scripts/multiplayer_manager.gd
@@ -14,10 +14,19 @@ func create_LAN_server():
 	$port.text = "port: " + str(port)
 	multiplayer.peer_connected.connect(spawn_player)
 	$main_menu.queue_free()
+	spawn_player(multiplayer.get_unique_id())
 	add_child(preload("res://prefabs/main_level.tscn").instantiate())
 
 func create_client():
-	pass
+	if $main_menu/port_input.text.is_valid_int():
+		var port = int($main_menu/port_input.text)
+		enet_peer.create_client("localhost", port)
+		multiplayer.multiplayer_peer = enet_peer
+		$main_menu.queue_free()
+	else:
+		$main_menu/error_message.hide()
+		await get_tree().create_timer(1).timeout
+	
 	
 func spawn_player(peer_id):
 	var player = preload("res://prefabs/player.tscn").instantiate()
