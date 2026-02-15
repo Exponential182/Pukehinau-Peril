f9b2b9ec91c0dfaaae5d84766b0fb4bc127884de
Flamerider347 <flamerider995@gmail.com>
2026-02-16 11:10:16 +1300

    good

--- scripts/vertical_puzzle.gd
+++ scripts/vertical_puzzle.gd
@@ -3,6 +3,9 @@ extends Node2D
 @onready var progress_bar = $bar/ProgressBar
 @onready var bar = $bar
 @onready var offset = $bar/ProgressBar.position
+@onready var combo_counter = $combo_counter
+@onready var combo_text = $combo_counter/combo_count
+@onready var combo_timer = $combo_timer
 var in_range = false
 var speed = 10
 var direction = 1
@@ -16,6 +19,9 @@ var shake_duration = 0.0
 var shake_timer = 0.0
 var rand_scale = 0.1
 var original_offset = Vector2(1470,530)
+var combo = 0
+var can_combo = true
+var combo_multiplier = 1
 
 
 # Called when the node enters the scene tree for the first time.
@@ -30,31 +36,54 @@ func _physics_process(delta: float) -> void:
 		shake_timer -= delta
 		$bar.scale =  Vector2(1,1) + Vector2(rand_scale,rand_scale
 		) * shake_strength/35 * (shake_timer / shake_duration)
-		$bar.rotation_degrees = 0 + randf_range(-1, 1) * shake_strength * (shake_timer / shake_duration)
+		$bar.rotation_degrees = 0 + randf_range(-1, 1) * 0.5 * shake_strength * (shake_timer / shake_duration)
 	else:
 		offset = Vector2.ZERO
 	win_area.position.y += speed * direction
 	var area_position = win_area.position.y
 	if min_height > area_position or max_height < area_position:
 		direction *= -1
-	if Input.is_action_just_pressed("smack"):
+	if Input.is_action_just_pressed("smack") and can_combo:
+		combo_multiplier = 1 + (0.1*combo)
+		can_combo = false
+		combo_timer.start(0.7)
 		speed = 0
 		var win_position = win_area.position.y
 		in_range = false
 		if min_win < win_position and win_position < max_win:
 			in_range = true
 		if in_range:
-			progress_bar.value = (progress_bar.value * 1.1) + 1
-			shake(0.5 * progress_bar.value, 1.0)
+			combo += 1
+			var progress_adition = combo_multiplier * (0.1 * progress_bar.value + 1) + 4
+			print(progress_adition)
+			progress_bar.value += progress_adition
+			shake(0.5 * progress_bar.value * combo_multiplier, 1.0)
+			if progress_bar.value >= 100:
+				$win.show()
+				await get_tree().create_timer(5).timeout
+				get_tree().reload_current_scene()
 		else:
 			progress_bar.value *= 0.9
-		await get_tree().create_timer(0.5).timeout
-		speed = base_speed + (base_speed*progress_bar.value/100)
+			combo = 0
+		combo_text.text = "COMBO
+		X" + str(combo)
+		$speed_timer.start(0.5)
+
+
+		
 	
 func shake(strength: float, duration: float):
-	if progress_bar.value > 50:
+	if combo > 2:
 		rand_scale = (0.01*(progress_bar.value-50))
 		shake_strength = strength
 		shake_duration = duration
 		shake_timer = duration
 		original_offset = Vector2(1470,530) + offset
+
+
+func _on_combo_timer_timeout() -> void:
+	can_combo = true
+
+
+func _on_speed_timer_timeout() -> void:
+	speed = base_speed * combo_multiplier * 3
