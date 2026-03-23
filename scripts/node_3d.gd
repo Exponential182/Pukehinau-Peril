30500e213a13145491c49f7247bf1a4dc4594149
Flamerider347 <flamerider995@gmail.com>
2026-03-23 16:06:25 +1300

    Added very uncopyrighted music
    + Bug fixes

--- scripts/node_3d.gd
+++ scripts/node_3d.gd
@@ -1,7 +1,19 @@
 extends Node3D
+
+var transitioning := false
+var speed = 2
+var can_speed = true
 func _physics_process(delta: float) -> void:
-	$scrolltext.position.y += delta
-	if $scrolltext.position.y > 50:
+	if Input.is_action_pressed("smack") and can_speed:
+		speed = 2
+	else:
+		speed = 1
+	$scrolltext.position.y += delta * speed
+	$audio_stream_player_3d.pitch_scale = 1* speed
+	if $scrolltext.position.y > 55 and not transitioning:
+		transitioning = true
+		can_speed = false
 		$animation_player.play("fade")
+		$UI/label.hide()
 		await $animation_player.animation_finished
 		get_tree().change_scene_to_file("res://prefabs/main_menu.tscn")
